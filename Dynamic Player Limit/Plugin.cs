011f36da31eca78dea1b825d884eadc03af1d44c
Kr4ken <Kr4kenKT@gmail.com>
2017-04-07 15:37:07 -0500

    Fixes

--- Dynamic Player Limit/Plugin.cs
+++ Dynamic Player Limit/Plugin.cs
@@ -13,8 +13,10 @@ namespace Kr4ken.DPL
             if ((DateTime.Now - lastCalled).TotalSeconds > 1)
             {
                 lastCalled = DateTime.Now;
-                if (Provider.clients.Count == Provider.maxPlayers + 2 && Provider.maxPlayers + 12 < 255)
+                if (Provider.clients.Count >= Provider.maxPlayers - 2 && Provider.maxPlayers + 12 < 255)
                     Provider.maxPlayers = (byte)(Provider.maxPlayers + 12);
+                else if (Provider.clients.Count <= Provider.maxPlayers - 14)
+                    Provider.maxPlayers = (byte)(Provider.maxPlayers - 12);
                 /*if (Provider.clients.Count > 22 && Provider.maxPlayers == 24)
                     Provider.maxPlayers = 36;
                 else if (Provider.clients.Count > 34 && Provider.maxPlayers == 36)
